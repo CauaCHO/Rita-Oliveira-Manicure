a18244755bea8ec3ae2511ec78a75d69bdfbb664
Cauã H. Oliveira <151974421+CauaCHO@users.noreply.github.com>
2026-06-06 18:49:10 -0300

    Evita cache antigo no PWA

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,10 +1,18 @@
-const CACHE_NAME = 'agenda-manicure-v1';
-const ASSETS = [
-  './', './index.html', './css/style.css', './js/utils.js', './js/storage.js', './js/agenda-publica.js', './manifest.json'
-];
-self.addEventListener('install', event => {
-  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)).catch(()=>{}));
+const CACHE_NAME = 'agenda-manicure-v2';
+
+self.addEventListener('install', () => {
+  self.skipWaiting();
 });
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys()
+      .then(keys => Promise.all(keys.map(key => caches.delete(key))))
+      .then(() => self.clients.claim())
+  );
+});
+
 self.addEventListener('fetch', event => {
-  event.respondWith(caches.match(event.request).then(cached => cached || fetch(event.request)));
+  if (event.request.method !== 'GET') return;
+  event.respondWith(fetch(event.request));
 });
